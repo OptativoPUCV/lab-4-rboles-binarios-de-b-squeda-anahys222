08bd9874cea7b487439619af5d322174266871f1
Your Name <you@example.com>
2025-05-03 18:24:45 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -155,6 +155,16 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
 
 Pair * upperBound(TreeMap * tree, void* key) {
+    TreeNode *node = tree->root;
+    TreeNode *ub_node = NULL;
+    while (node != NULL) {
+        if (is_equal(tree, key, node->pair->key)) {
+            tree->current = node;
+            return node->pair;
+        }
+
+
+
     return NULL;
 }
 
